9a56b58a691aec0c2f1f7fa0ddb8d75e6167caaa
Ignacio Huanca <ignacarp06@gmail.com>
2025-04-12 18:43:46 -0300

    ejercicio 17

---
+++ src/ejercicio17.js
@@ -0,0 +1,13 @@
+let s = prompt("Ingrese un string: ");
+
+const contarLetraA = (s) => {
+    let cnt = 0;
+    for(let i = 0; i < s.length; i++)
+        if(s[i] == 'a')
+            cnt++;
+    return cnt;
+}
+
+let cnt = contarLetraA(s);
+
+console.log("La cantidad de veces que aparece 'a' es: " + cnt);
