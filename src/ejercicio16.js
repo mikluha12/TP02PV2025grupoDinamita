1a9a33532a13eb5aa70a890a96e47efa36b3ac4f
Ignacio Huanca <ignacarp06@gmail.com>
2025-04-12 18:37:59 -0300

    ejercicio16

---
+++ src/ejercicio16.js
@@ -0,0 +1,19 @@
+let l, r;
+do{
+    l = Number(prompt("Inicio: "));
+    r = Number(prompt("Fin: "));
+    if(l > r){
+        console.log(l + " debe ser menor que " + r);
+    }
+}while(l > r);
+
+const sumarRango = (l, r) => {
+    let sum = 0;
+    for(let i = l; i <= r; i++)
+        sum += i;
+    return sum;
+}
+
+let sum = sumarRango(l, r);
+
+console.log("La suma total del rango [" + l + ", " + r + "] es: " + sum);
