761f7435998b9d489ea911060b0e2de40e5f6a30
matias12131 <velazquezmatiasclass@gmail.com>
2025-04-12 18:23:30 -0300

    ejercicio3 terminado

---
+++ src/ejercicio3.js
@@ -0,0 +1,8 @@
+let radio = 7.5;
+const pi=3.1416;
+
+let area1 = pi* Math.pow(radio,2);
+let area2 = Math.PI* Math.pow(radio,2);
+
+console.log("El area de un circulo cuyo radio vale 7,5 es: "+area1+" (con pi=3,1416)");
+console.log("El area de un circulo cuyo radio vale 7,5 es: "+area2+" (con Math.PI)");
