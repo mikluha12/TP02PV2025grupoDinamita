7192cec4f2d2b53bd1ba661ba981c1afa3bbbb6a
Yato <darkflamesmage23@gmail.com>
2025-04-13 03:51:43 -0300

    Ejercicios 5, 9 y 14 agregados

---
+++ src/Js/ejercicio5.js
@@ -0,0 +1,8 @@
+let horas = 20
+let minutos = 32
+
+let Hsecs = horas * 3600
+let Msecs = minutos * 60
+
+console.log(horas," horas convertidas a segundos: ",Hsecs)
+console.log(minutos," minutos convertidos a segundos: ",Msecs)
